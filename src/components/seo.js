2127d08275853140a6bb694963f2a2d5ff0acf5c
site-B <livebyit@protonmail.com>
2021-01-31 17:09:54 +0100

    seo fixing

--- src/components/seo.js
+++ src/components/seo.js
@@ -57,7 +57,7 @@ function SEO({ description, lang, image, meta, title }) {
         },
         {
           name: `twitter:image`,
-          content: "https://elegant-bassi-d0fe18.netlify.app/twitterimage.png",
+          content: "https://elegant-bassi-d0fe18.netlify.app/logo.png",
         },
         {
           name: `twitter:creator`,
