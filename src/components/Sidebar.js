88136f63e9165c990133b0ae6ab8162cef3e4267
site-B <livebyit@protonmail.com>
2021-02-10 16:43:04 +0100

    random quote api

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,117 +1,109 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Card, CardTitle, CardText, CardBody } from 'reactstrap'; 
 import { Link, graphql, StaticQuery } from 'gatsby'; 
 import Img from 'gatsby-image'; 
 import YouTube from 'react-youtube'; 
+import axios from 'axios'; 
 
 
 const opts = {
-    height: '100%', 
+    height: '100%',
     width: '100%'
 }
 
-class Sidebar extends React.Component {
 
-    componentDidMount() {
-        fetch('https://api.nytimes.com/svc/topstories/v2/world.json?api-key=xojrhqbxFQ7T4FGGyyipF7vw7n2xAGTr') //url here + api key according to nyt api docs
-        
-        .then(response => {
-            // console.log(response);  1. to check if this works, and it works here; 
-            // consider that doing this in gatsby, it also pulls static data from graphql and/or pulls out data from a dynamic source and makes it static during the build process; more info in the gatsby doc 
 
-            return response.json()
-        }).then(json => {
-            //console.log(json)  2. check out what we receive, so we can decide on what we need to render here 
-            this.setState({ // 3. setting state; in this case the name is results because it's the name it uses, could be anything 
-                fetchedData: json.results,
-                loading: false, 
+function Sidebar() {
+    const [posts, setPosts] = useState([])
+    const x = Math.floor((Math.random() * 1642) + 1);
+
+    useEffect(() => {
+        axios
+            .get('https://type.fit/api/quotes')
+            .then(res => {
+                console.log(res)
+                setPosts(res.data)
             })
-        })
-    }
+            .catch(error => {
+                console.log(error)
+            })
+    }, [])
+    
 
+    return (
+        <div>
+    
+            <Card className='cardBody'>
+                <CardTitle className='sidebarTitle text-center mb-3'>
+                    Today's quote
+                </CardTitle>
+                {
+                    posts.map(
+                        post => (
+                        <ul>{post.text} <p></p>{post.author}</ul>
+                    )).slice(x, x + 1)
+                }
+            </Card>
 
-    render(){
-
-        // 4. 
-        const {fetchedData} = this.state
-
-        return (
-            <div>
-
-
-
-                <Card>
-                    <CardBody className='cardBody'>
-                        <CardTitle className='sidebarTitle text-center mb-3'>
-                            Today's video
-                        </CardTitle>
-                        <CardText className='sidebarText'>
-                            The first seconds of Mr. Self Destruct (from NIN's <a href='https://open.spotify.com/album/3nJnyDV8fwFpffo0EyHQto?autoplay=true' target='_blank' rel="noreferrer">The Downward Spiral</a>) belong to 1971's <a href='https://www.imdb.com/title/tt0066434/?ref_=nv_sr_srsg_0' target='_blank' rel="noreferrer">THX 1138</a>. The first film by George Lucas. 
-                        </CardText>
-                        <YouTube opts={opts}
-                            videoId="cOppY-p7gQI"
-                        />
-                    </CardBody>
-                </Card>
-                <Card>
-                    <CardBody className='cardBody'>
-                        <CardTitle className='sidebarTitle text-center mb-3'>
-                            TIL
-                        </CardTitle>
-                        <CardText className='sidebarText'>
-                        The Future Refused To Change. 
-                        </CardText>
-                        <YouTube opts={opts}
-                            videoId="cMP9PPOoFiw"
-                        />
-                    </CardBody>
-                </Card>
-                <Card>
-                    <CardBody className='cardBody'>
-                        <CardTitle className='sidebarTitle text-center mb-3'>
-                            Recent Posts
-                        </CardTitle>
-                        <StaticQuery 
-                            query={sidebarQuery} 
-                            render={(data) => (
-                                <div>
-                                    {
-                                        data.allMarkdownRemark.edges.map(({node}) => (
-                                            <Card key={node.id}>
+        <Card>
+            <CardBody className='cardBody'>
+                <CardTitle className='sidebarTitle text-center mb-3'>
+                    Today's video
+                </CardTitle>
+                <CardText className='sidebarText'>
+                    The first seconds of Mr. Self Destruct (from NIN's <a href='https://open.spotify.com/album/3nJnyDV8fwFpffo0EyHQto?autoplay=true' target='_blank' rel="noreferrer">The Downward Spiral</a>) belong to 1971's <a href='https://www.imdb.com/title/tt0066434/?ref_=nv_sr_srsg_0' target='_blank' rel="noreferrer">THX 1138</a>. The first film by George Lucas. 
+                </CardText>
+                <YouTube opts={opts}
+                    videoId="cOppY-p7gQI"
+                />
+            </CardBody>
+        </Card>
+        <Card>
+            <CardBody className='cardBody'>
+                <CardTitle className='sidebarTitle text-center mb-3'>
+                    TIL
+                </CardTitle>
+                <CardText className='sidebarText'>
+                The Future Refused To Change. 
+                </CardText>
+                <YouTube opts={opts}
+                    videoId="cMP9PPOoFiw"
+                />
+            </CardBody>
+        </Card>
+        <Card>
+            <CardBody className='cardBody'>
+                <CardTitle className='sidebarTitle text-center mb-3'>
+                    Recent Posts
+                </CardTitle>
+                <StaticQuery 
+                    query={sidebarQuery} 
+                    render={(data) => (
+                        <div>
+                            {
+                                data.allMarkdownRemark.edges.map(({node}) => (
+                                    <Card key={node.id}>
+                                        <Link to={node.fields.slug}>
+                                            <Img fluid={node.frontmatter.image.childImageSharp.fluid} />
+                                        </Link>
+                                        <CardBody className='cardBody'>
+                                            <CardTitle className='text-center mb-3'>
                                                 <Link to={node.fields.slug}>
-                                                    <Img fluid={node.frontmatter.image.childImageSharp.fluid} />
+                                                    {node.frontmatter.title}
                                                 </Link>
-                                                <CardBody className='cardBody'>
-                                                    <CardTitle className='text-center mb-3'>
-                                                        <Link to={node.fields.slug}>
-                                                            {node.frontmatter.title}
-                                                        </Link>
-                                                    </CardTitle>
-                                                </CardBody>
-                                            </Card>
-                                        ))
-                                    }
-                                </div>
-                        )}/>
-                    </CardBody>
-                </Card>
-                <Card>
-                    <CardBody className='cardBody'>
-                        <CardTitle className='sidebarTitle text-center mb-3'>
-                                News of the World
-                        </CardTitle>
-                        {
-                            fetchedData.map(article => (
-                                <ul class='news-title'>{article.title}. <a href={article.url} target='_blank'>more...</a></ul>
-                            )).slice(0, 10)
-                        }
-                        <h6>Made with NYT's API</h6>
-                    </CardBody>
-                </Card>
-            
-            </div> 
-        )
-    }
+                                            </CardTitle>
+                                        </CardBody>
+                                    </Card>
+                                ))
+                            }
+                        </div>
+                )}/>
+            </CardBody>
+        </Card>
+
+    
+    </div> 
+    )
 }
 
 const sidebarQuery = graphql`
@@ -140,3 +132,24 @@ const sidebarQuery = graphql`
 `
 
 export default Sidebar; 
+
+/*API FETCH CALL CODE: 
+
+    componentDidMount() {
+        fetch('https://api.nytimes.com/svc/topstories/v2/world.json?api-key=xojrhqbxFQ7T4FGGyyipF7vw7n2xAGTr') //url here + api key according to nyt api docs
+        
+        .then(response => {
+            // console.log(response);  1. to check if this works, and it works here; 
+            // consider that doing this in gatsby, it also pulls static data from graphql and/or pulls out data from a dynamic source and makes it static during the build process; more info in the gatsby doc 
+
+            return response.json()
+        }).then(json => {
+            //console.log(json)  2. check out what we receive, so we can decide on what we need to render here 
+            this.setState({ // 3. setting state; in this case the name is results because it's the name it uses, could be anything 
+                fetchedData: json.results,
+                loading: false, 
+            })
+        })
+    }
+    
+    */ 
